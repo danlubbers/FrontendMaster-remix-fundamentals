37ca95e9a919cc3885bdd76c5640b8f761822f10
Dan Lubbers <lubbers.dan@gmail.com>
2022-11-05 15:27:03 -0400

    Exercise 02 Completed

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,26 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPostListItems } from "~/models/post.server";
+
+export const loader = async () => {
+  const posts = await getPostListItems();
+  return json({ posts });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
