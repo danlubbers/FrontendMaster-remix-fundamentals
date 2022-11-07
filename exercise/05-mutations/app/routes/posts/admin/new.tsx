0779b3bfda1aed53b69f17e0bef48d13079dfb0f
Dan Lubbers <lubbers.dan@gmail.com>
2022-11-07 10:34:53 -0500

    Exercise 05 Completed

--- exercise/05-mutations/app/routes/posts/admin/new.tsx
+++ exercise/05-mutations/app/routes/posts/admin/new.tsx
@@ -1,27 +1,67 @@
+import { Form, useActionData } from "@remix-run/react";
+import type { ActionArgs } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
+import { json } from "@remix-run/node";
+import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
+
 // 🐨 implement the action function here.
 // 1. accept the request object
-// 2. get the formData from the request
-// 3. get the title, slug, and markdown from the formData
-// 4. call the createPost function from your post.model.ts
-// 5. redirect to "/posts/admin".
+export const action = async ({ request }: ActionArgs) => {
+  // 2. get the formData from the request
+  const formData = await request.formData();
+  console.log("formData", formData);
+  // 3. get the title, slug, and markdown from the formData
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required!",
+    slug: slug ? null : "Slug is required!",
+    markdown: markdown ? null : "Markdown is required!",
+  };
+
+  const hasErrors = Object.values(errors).some(Boolean);
+  if (hasErrors) {
+    return json({ errors });
+  }
+
+  invariant(typeof title === "string", "Title must be a string");
+  invariant(typeof slug === "string", "Slug must be a string");
+  invariant(typeof markdown === "string", "Markdown must be a string");
+
+  // 4. call the createPost function from your post.model.ts
+  createPost({ title, slug, markdown });
+
+  // 5. redirect to "/posts/admin".
+  return redirect(`/posts/${slug}`);
+};
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPost() {
+  const actionData = useActionData<typeof action>();
   return (
     // 🐨 change this to a <Form /> component from @remix-run/react
     // 🐨 and add method="post" to the form.
-    <form>
+    <Form method="post">
       <p>
         <label>
           Post Title:{" "}
           <input type="text" name="title" className={inputClassName} />
+          {actionData?.errors?.title && (
+            <em className="text-red-600">{actionData?.errors.title}</em>
+          )}
         </label>
       </p>
       <p>
         <label>
           Post Slug:{" "}
           <input type="text" name="slug" className={inputClassName} />
+          {actionData?.errors?.slug && (
+            <em className="text-red-600">{actionData?.errors.slug}</em>
+          )}
         </label>
       </p>
       <p>
@@ -33,6 +73,9 @@ export default function NewPost() {
           name="markdown"
           className={`${inputClassName} font-mono`}
         />
+        {actionData?.errors?.markdown && (
+          <em className="text-red-600">{actionData?.errors.markdown}</em>
+        )}
       </p>
       <p className="text-right">
         <button
@@ -42,6 +85,6 @@ export default function NewPost() {
           Create Post
         </button>
       </p>
-    </form>
+    </Form>
   );
 }
